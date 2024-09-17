e774c8b25da7220ca1532048cd80a5099448054c
Justin Ebanks <justinebanks580@gmail.com>
2024-09-17 19:58:05 -0400

    First Commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+	import { ref } from "vue";
+
+	const message = ref("Hello World");
+
+	const changeMsg = () => {
+		message.value = "New Message";
+	}
+</script>
+
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<button @click="changeMsg()">Change Message</button>
+	</div>
+</template>
+
+<style scope></style>
