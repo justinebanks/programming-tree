c819bdbcd36997edb1b1653f44769e82fa45619b
Justin Ebanks <justinebanks580@gmail.com>
2024-09-18 19:15:09 -0400

    Adds A Basic Starter Page

---
+++ frontend/src/components/HeroPage.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+    title: {
+        type: String,
+        default: "Hello World"
+    },
+    text: {
+        type: String,
+        default: "Lorem ipsum dolor"
+    }
+});
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="text">
+            <h1>{{ title }}</h1>
+            <p>{{ text }}</p>
+        </div>
+        <img src="https://i.giphy.com/media/dxCT5ok6OM7MS5zAqA/giphy.gif" alt="Image">
+    </div>
+</template>
+
+<style scoped>
+    div.container {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        border-bottom: 2px solid var(--fav-color);
+        height: 500px;
+    }
+
+    div.text {
+        /* border: 2px solid black; */
+        width: 500px;
+    }
+
+    img {
+        border-radius: 50%;
+        border: 2px solid var(--fav-color);
+        box-shadow: 50px 50px 50px;
+    }
+
+    h1 {
+        color: white;
+        font-size: 50px;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+    }
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,55 @@
+
+<template>
+    <ul>
+        <li>Programming Tree</li>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/tree">Tree</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+</template>
+
+<style scope>
+    ul {
+        display: flex;
+        justify-content: right;
+        list-style-type: none;
+        
+        border-bottom: 2px solid var(--fav-color);
+        padding: 20px;
+        
+    }
+
+    ul:hover {
+        background-color: hsla(200, 100%, 10%, 0.7);
+        transition: 0.3s;
+    }
+
+    ul > li:first-child {
+        /* justify-self: flex-start; */
+        margin-right: 500px;
+        color: white;
+        font-size: 25px;
+        font-weight: bolder;
+    }
+
+    ul > li:not(:first-child) {
+        text-decoration: none;
+        padding: 15px;
+        border: 1px solid black;
+        margin-left: 60px;
+        background-color: var(--fav-color);
+        width: 100px;
+        text-align: center;
+        border-radius: 40px;
+    }
+
+    ul > li:not(:first-child):hover {
+        background-color: hsl(200, 100%, 30%);
+    }
+
+    ul > li > a {
+        text-decoration: inherit;
+        color: white;
+        font-weight: bold;
+    }
+</style>
