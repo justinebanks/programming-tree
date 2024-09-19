2a2c96bdf6dd7362024c073acddcb62668a692b3
SpaceAgeX <adrian.aryeh@gmail.com>
2024-09-18 20:12:57 -0400

    attempted backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+const PORT = process.env.PORT || 80;
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', index.html));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
