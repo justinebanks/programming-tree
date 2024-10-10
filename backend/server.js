ca8c000bfe14393cac03e13e3d62123468086f02
Justin Ebanks <justinebanks580@gmail.com>
2024-10-10 17:13:21 -0400

    Adds API Endpoints for Creating And Deleting Segments within a Node

--- backend/server.js
+++ backend/server.js
@@ -44,13 +44,20 @@ fs.readFile(path.join(__dirname, '/config.ini'), 'utf-8', (err, data) => {
 })
 
 
-
 const app = express();
 
 
 const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.json());
 app.use(cors());
+
+
+const getNode = () => {
+    
+}
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist', index.html));
@@ -69,9 +76,64 @@ app.get("/nodes/:id", async (req, res) => {
         res.json(results[0]);
     }
     else {
-        res.status(404).json({error: "Generic Error Message"});
+        res.status(404).json({error: "Node Not Found"});
     }
     
+});
+
+app.post("/nodes/:id", async (req, res) => {
+    const { text, isCode } = req.body;
+    const [results, _] = await sequelize.query('SELECT * FROM nodes WHERE id = ?;', { replacements: [req.params["id"]] });
+    console.log(results);
+
+    if (results.length > 0) {
+        const node = results[0];
+        let encSegments = JSON.parse(decodeURI(node.segments));
+
+        let plainString = `:${isCode ? 'code' : 'text'}:${text}`;
+        let encodedFinalText = btoa(plainString);
+        encSegments.push(encodedFinalText);
+
+        let newSegments = encodeURI(JSON.stringify(encSegments));
+
+        try {
+            await sequelize.query('UPDATE nodes SET segments = ? WHERE id = ?;', {replacements: [newSegments, req.params['id']]});
+            res.json({msg: "Success"});
+        }
+        catch (err) {
+            res.json({ msg: err });
+        }
+     
+    }
+    else {
+        res.status(404).json({error: "Node Not Found"});
+    }
+})
+
+app.delete("/nodes/:id", async (req, res) => {
+    const { index } = req.body;
+    const [results, _] = await sequelize.query('SELECT * FROM nodes WHERE id = ?;', { replacements: [req.params["id"]] });
+    console.log(results);
+
+    if (results.length > 0) {
+        const node = results[0];
+        let encSegments = JSON.parse(decodeURI(node.segments));
+        encSegments.splice(index, 1);
+
+        let newSegments = encodeURI(JSON.stringify(encSegments));
+
+        try {
+            await sequelize.query('UPDATE nodes SET segments = ? WHERE id = ?;', {replacements: [newSegments, req.params['id']]});
+            res.json({msg: "Success"});
+        }
+        catch (err) {
+            res.json({ msg: err });
+        }
+     
+    }
+    else {
+        res.status(404).json({error: "Node Not Found"});
+    }
 });
 
 
